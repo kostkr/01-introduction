4425b07d305f6d028c763f0dea73721bbbd3fae7
Miroslav Dovger <miroslavdovger@gmail.com>
2023-10-05 22:24:28 +0200

    QuadraticEquation and Reverser

--- src/main/java/uj/wmii/pwj/introduction/QuadraticEquation.java
+++ src/main/java/uj/wmii/pwj/introduction/QuadraticEquation.java
@@ -3,8 +3,18 @@ package uj.wmii.pwj.introduction;
 public class QuadraticEquation {
 
     public double[] findRoots(double a, double b, double c) {
-        return null;
-    }
+        double discriminant = b * b - 4 * a * c;
 
+        if (discriminant > 0) {
+            double root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+            double root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+            return new double[]{root1, root2};
+        } else if (discriminant == 0) {
+            double root = -b / (2 * a);
+            return new double[]{root};
+        } else {
+            return new double[]{};
+        }
+    }
 }
 
--- src/main/java/uj/wmii/pwj/introduction/Reverser.java
+++ src/main/java/uj/wmii/pwj/introduction/Reverser.java
@@ -3,11 +3,23 @@ package uj.wmii.pwj.introduction;
 public class Reverser {
 
     public String reverse(String input) {
-        return null;
+        if(input == null)
+            return null;
+        else {
+            StringBuilder sb = new StringBuilder(input.trim());
+            sb.reverse();
+            return sb.toString();
+        }
     }
 
     public String reverseWords(String input) {
-        return null;
+        String[] words = input.split("\\s+");
+        StringBuilder sb = new StringBuilder();
+        for (int i = words.length - 1; i >= 0; i--) {
+            sb.append(words[i]);
+            if(i > 0) sb.append(" ");
+        }
+        return sb.toString();
     }
 
 }
