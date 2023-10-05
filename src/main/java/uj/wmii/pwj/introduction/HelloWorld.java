820ef33f4b14817ecf92ffcc0926261106e38f99
Miroslav Dovger <miroslavdovger@gmail.com>
2023-10-05 20:50:07 +0200

    HelloWorld

--- src/main/java/uj/wmii/pwj/introduction/HelloWorld.java
+++ src/main/java/uj/wmii/pwj/introduction/HelloWorld.java
@@ -2,5 +2,12 @@ package uj.wmii.pwj.introduction;
 
 public class HelloWorld {
     public static void main(String[] args) {
+        if (args.length > 0) {
+            for (String arg : args) {
+                System.out.print(arg + "\n");
+            }
+        } else {
+            System.out.println("No input parameters provided");
+        }
     }
 }
